cebd666fc1b1daeb8fd42bb10ed9f9826e4f3caf
MilanStalwart <milan.stalwartdigital@gmail.com>
2024-06-10 17:07:18 +0530

    Added Apps Code

--- Node App Test/Shopify Node App DB/deleteProduct.js
+++ Node App Test/Shopify Node App DB/deleteProduct.js
@@ -4,10 +4,10 @@ import { restResources } from "@shopify/shopify-api/rest/admin/2023-10";
 import express from "express";
 
 const shopify = shopifyApi({
-  apiSecretKey: "4ccac5840c92849ffdd7cb05b378de9e",
+  ASK: "4ccac5840c92849ffdd7cb05b378de9e",
   apiVersion: LATEST_API_VERSION,
   isCustomStoreApp: true,
-  adminApiAccessToken: "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",
+  AT: "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",
   isEmbeddedApp: false,
   hostName: "milan-r-dev.myshopify.com",
   restResources,
--- b2b-app/b2b-app/config/config.js
+++ b2b-app/b2b-app/config/config.js
@@ -10,7 +10,7 @@ const app = express();
 // import { restResources } from "@shopify/shopify-api/rest/admin/2023-10";
 // const shopify = shopifyApi({
 //   apiKey: "564787232f953504bf617e4975ff5fc6",
-//   apiSecretKey: "53076a6fbaa0eaf81e75f738eb0a557f",
+//   ASK: "53076a6fbaa0eaf81e75f738eb0a557f",
 //   hostName: "2bd5-2401-4900-1c80-28ee-8411-8f92-ad2d-62ab.ngrok-free.app",
 //   adminApiAccessToken: "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",
 //   isCustomStoreApp: true,
